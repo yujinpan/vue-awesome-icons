00c381fb1cd6e94665843c638bd689e8b813608a
yujinpan <1192878390@qq.com>
2020-08-31 14:01:47 +0800

    chore: 修改规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,17 @@ module.exports = {
   extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
 
   rules: {
+    'prettier/prettier': [
+      'error',
+      // 这里自定义 prettier 的规则
+      {
+        singleQuote: true,
+        arrowParens: 'always',
+        semi: true
+      }
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     eqeqeq: ['error', 'always']
   },
 
